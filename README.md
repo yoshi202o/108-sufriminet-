b11db6d6aec0a09b0d7d556872e497c6d927e2c2
yoshi202o <90802548+yoshi202o@users.noreply.github.com>
2023-04-18 19:54:54 -0600

    Add files via upload

---
+++ README.md
@@ -0,0 +1,8 @@
+# PRO_C118_AA1_V1
+IMPLEMENTACIÓN DE APLICACIÓN WEB: Diario digital  
+Python. Flask. jQuery.  
+  
+Plan de la clase.  
+Plantilla del código del alumno.  
+  
+### Texto en inglés: PRO-C118-Student-Boilerplate-Code
---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, request, jsonify
+from model_prediction import *
+
+app = Flask(__name__)
+
+text=""
+predicted_emotion=""
+predicted_emotion_img_url=""
+
+@app.route("/")
+def home():
+    entries = show_entry()
+    return render_template("index.html", entries=entries)
+    
+
+@app.route("/predict-emotion", methods=["POST"])
+def predict_emotion():
+    input_text = request.json.get("text")
+    if not input_text:
+        return jsonify({
+            "status": "error",
+            "message": "¡Por favor, introduce algún texto para predecir la emoción!"
+        }), 400
+    else:
+        predicted_emotion, predicted_emotion_img_url = predict(input_text)                         
+        return jsonify({
+            "data": {
+                "predicted_emotion": predicted_emotion,
+                "predicted_emotion_img_url": predicted_emotion_img_url
+            },
+            "status": "success"
+        }), 200
+        
+#Escribir el código para la API aquí
+
+                
+if __name__ == "__main__":
+    app.run(debug=True)
+
